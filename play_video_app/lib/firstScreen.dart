8c435f1b649263e11acb0f5445ab6ca3e3941eb1
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-09 15:23:45 +0200

    Some updates in the application

--- play_video_app/lib/firstScreen.dart
+++ play_video_app/lib/firstScreen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:video_player/video_player.dart';
 
 class firstScreen extends StatefulWidget {
-  const firstScreen({super.key});
+  const firstScreen();
 
   @override
   State<firstScreen> createState() => _State();
 }
 
 class _State extends State<firstScreen> {
-  @override
+  List<String> videoUrls = [];
+  VideoPlayerController _controller;
 
-  //Inicio do metodo responsavel por fazer o get de todos os dados da API
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  // Início do método responsável por fazer o GET de todos os dados da API
   void fetchData() async {
     final response = await http.get(
         'https://raw.githubusercontent.com/bikashthapa01/myvideos-android-app/master/data.json');
@@ -23,18 +33,62 @@ class _State extends State<firstScreen> {
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Inicio do appBar
       appBar: AppBar(
-        title: Text('Video Application'),
+        title: Text('Video List'),
       ),
-      //Inicio do container
-      body: Container(
-        child: Column(
-          children: [
-            Text('Ola Mundo'),
-          ],
+      body: ListView.builder(
+        itemCount: videoUrls.length,
+        itemBuilder: (context, index) {
+          _controller = VideoPlayerController.network(videoUrls[index]);
+          _controller.initialize();
+
+          return ListTile(
+            title: Text('Video $index'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VideoPlayerScreen(controller: _controller),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayerScreen({this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (controller.value.isPlaying) {
+            controller.pause();
+          } else {
+            controller.play();
+          }
+        },
+        child: Icon(
+          controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
