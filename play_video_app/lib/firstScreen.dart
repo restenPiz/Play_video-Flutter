795be03d159c610c5c15b4db2639d7c6cb0679f0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-09 14:56:38 +0200

    SOme updates in the application

--- play_video_app/lib/firstScreen.dart
+++ play_video_app/lib/firstScreen.dart
@@ -9,6 +9,20 @@ class firstScreen extends StatefulWidget {
 
 class _State extends State<firstScreen> {
   @override
+
+  //Inicio do metodo responsavel por fazer o get de todos os dados da API
+  void fetchData() async {
+    final response = await http.get(
+        'https://raw.githubusercontent.com/bikashthapa01/myvideos-android-app/master/data.json');
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        videoUrls = List<String>.from(data['videos']);
+      });
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       //Inicio do appBar
