6c7414692749fe11d3f35ef615fe7de277762796
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-09 14:32:57 +0200

    Some updates in the application

---
+++ play_video_app/lib/firstScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class firstScreen extends StatefulWidget {
+  const firstScreen({super.key});
+
+  @override
+  State<firstScreen> createState() => _State();
+}
+
+class _State extends State<firstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
