d8bd7dbb81ce274c009762b710058f9c2fceb040
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-10 09:14:31 +0200

    Some updates in the application

--- play_video_app/lib/firstScreen.dart
+++ play_video_app/lib/firstScreen.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Metodo responsavel por colocar o appDebuger como false
       debugShowCheckedModeBanner: false,
       home: FirstScreen(),
     );
