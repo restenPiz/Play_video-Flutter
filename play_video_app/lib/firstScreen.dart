a9bdec226db4732786c1da976ae5efea7ca3122c
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-10 15:45:14 +0200

    Add some comments in the source code

--- play_video_app/lib/firstScreen.dart
+++ play_video_app/lib/firstScreen.dart
@@ -58,6 +58,7 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //Incio do titulo do AppBar
       appBar: AppBar(
         title: Text('Resten - Play'),
       ),
